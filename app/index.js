3e5f9a971100aaad511dca4751ddcdafa939fb63
ChumaMqeke <cmqeke2401@gmail.com>
2024-03-28 13:28:29 +0200

    changes

--- app/index.js
+++ app/index.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native'; //these are react native components
+import { Stack,useRouter } from 'expo-router';
 
+import { COLORS, icons, images, SIZES } from '../constants';
+import { Nearbyjobs, ScreenHeaderBtn, Welcome } from '../components';
 const Home = () => {
+    const router = useState();
+
     return (
-        <View>
-            <Text>hello and welcome to my new app</Text>
-        </View>
+         //  allows you to show the content safely without the buttons appearing over it
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen 
+                options = {{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension = "60%" />
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={icons.profile} dimension = "100%" />
+                    ),
+                    headerTitle: ""
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
