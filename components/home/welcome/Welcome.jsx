5a3b215d226e0fb831059db7169eab173c9783cb
Tinaqustu <thinaqutsu8@gmail.com>
2024-03-28 21:49:26 +0200

    Search section

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,9 +12,11 @@ import {
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants';
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
 const Welcome = () => {
   const router = useRouter();
-
+  const [activeJobType, setActiveJobType] = useState('Full-time')
   return (
     <View>
       <View style={styles.continer}>
@@ -27,12 +29,44 @@ const Welcome = () => {
       </View>
 
       <View style={styles.searchContainer}>
-        <View styles={styles.searchWrapper}>
+        <View style={styles.searchWrapper}>
           <TextInput
+            //  value=''
              style={styles.searchInput}
+             onChange={() => {}}    //callback function
+             placeholder='What are you looking for?'
           />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
       </View>
+
+       <View style={styles.tabsContainer}> 
+        <FlatList // used when you want to render a list of items so that you can be able to scroll horizontally
+          data={jobTypes} // calling the job types from the arrays
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {  // this will show the selected item (job type button)
+                setActiveJobType(item);
+                router.push('/search/${item}') // It will search through items
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{ item }</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item} // hover on the text to read the description of it
+          contentContainerStyle={{ columnGap: SIZES.small}}
+          horizontal
+        />
+       </View>
+
     </View>
   )
 }
