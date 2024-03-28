86eaec3390f284989aec934a89046308045af968
ChumaMqeke <cmqeke2401@gmail.com>
2024-03-28 12:19:01 +0200

    app set up

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>hello and welcome to my new app</Text>
+        </View>
+    )
+}
+
+export default Home;  
