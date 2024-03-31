514af477c0a3a9b69e6dbae8efa5dc1b9cc89573
Tinaqustu <thinaqutsu8@gmail.com>
2024-03-31 21:19:09 +0200

    integrating active jobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -12,8 +12,18 @@ const PopularJobcard = ({ item, selectedJob, handleCardPress }) => {
     <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
       <Image 
       source={{ url: item.employer_logo }}
+      resizeMode='contain'
+      style={styles.logoImage}
       />
     </TouchableOpacity>
+    <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+    <View style={styles.inforContainer}>
+      <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+        {item.job_title}
+      </Text>
+
+      <Text style={styles.location}>{item.job_country}</Text>
+    </View>
   </TouchableOpacity>
  )
 }
